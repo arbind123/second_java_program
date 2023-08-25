e0102356b1dc769ebaa20c0b44da05e41e48145e
ARBIND KUMAR <41694631+arbind123@users.noreply.github.com>
2023-08-25 13:22:18 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo{
+	public static void main(String args[]){
+	System.out.println("hello java");
+	}
+
+}
+class Main{
+	public static void main(){
+	System.out.println("2nd helo java");
+	}
+}
