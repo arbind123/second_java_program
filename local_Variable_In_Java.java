7291a1a10b696ec9ea6a0a02de07e568fd43b540
ARBIND KUMAR <41694631+arbind123@users.noreply.github.com>
2023-08-26 17:43:14 +0530

    Add files via upload

---
+++ local_Variable_In_Java.java
@@ -0,0 +1,13 @@
+class local_Variable_In_Java{
+	public static void main(String args[]){
+	 int a =1;   //local variable must be insilize not insilize given error
+	System.out.println(a);
+	}
+}
+
+/*
+E:\JAVA_ALL_PROGRAM>javac local_Variable_In_Java.java
+
+E:\JAVA_ALL_PROGRAM>java local_Variable_In_Java
+1
+*/
