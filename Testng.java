4a654c3f776d723794a1df02e2b7bbebaffe3e35
ARBIND KUMAR <41694631+arbind123@users.noreply.github.com>
2023-08-26 17:21:03 +0530

    Add files via upload

---
+++ Testng.java
@@ -0,0 +1,33 @@
+class Testng{
+	
+static int a;   		//default value zero(0)
+static short b;		//default value zero(0)
+static long c;		//default value zero(0)
+static byte d;		//default value zero(0)
+static float e;		//default value zero(0)
+static double f;		//default value zero point zero(0.0)
+static String g;		//default value null
+	public static void main(String args[]){
+	System.out.println(a);
+	System.out.println(b);
+	System.out.println(c);
+	System.out.println(d);
+	System.out.println(e);
+	System.out.println(f);
+	System.out.println(g);
+	}
+}
+
+
+/*output
+E:\JAVA_ALL_PROGRAM>javac Testng.java
+
+E:\JAVA_ALL_PROGRAM>java Testng
+0
+0
+0
+0
+0.0
+0.0
+null
+*/
